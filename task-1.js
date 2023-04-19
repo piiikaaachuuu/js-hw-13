91046b6d4e5bf430075fa51051707c4d320042b1
Oleksandra Matiash-Rybachuk <69486420+piiikaaachuuu@users.noreply.github.com>
2023-04-19 14:49:26 +0300

    Add files via upload

---
+++ task-1.js
@@ -0,0 +1,30 @@
+// Написати програму, яка буде обчислювати площу прямокутника та квадрата,
+// використовуючи прототипи та прототипне наслідування.
+
+function figure() {}
+
+
+
+function rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+}
+rectangle.prototype = new figure();
+rectangle.prototype.area = function () {
+  return this.width * this.height;
+};
+var rect = new rectangle(5, 3);
+console.log(rect.area());
+
+
+
+function square(side) {
+  this.width = side;
+  this.height = side;
+}
+square.prototype = new figure();
+square.prototype.area = function () {
+  return this.width * this.height;
+};
+var squar = new square(4);
+console.log(squar.area());
